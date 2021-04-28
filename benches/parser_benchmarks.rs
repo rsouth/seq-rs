f5421135aaf801326cec112c0f0156870dfba2b9
Richard South <southy98@gmail.com>
2021-04-28 08:21:35 +0100

    Update parser_benchmarks.rs

--- benches/parser_benchmarks.rs
+++ benches/parser_benchmarks.rs
@@ -1,5 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use sequencer::{InteractionParser, ParticipantParser};
+use sequencer::parser::{InteractionParser, ParticipantParser};
 
 pub fn get_text() -> String {
     String::from(
